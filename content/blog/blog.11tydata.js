5b4138d86d29b6d3552907d3afd8faafd02b368d
Alex Nathanail <7809723+adnathanail@users.noreply.github.com>
2026-08-04 15:43:25 +0200

    Rewire author data directly into posts collection

--- content/blog/blog.11tydata.js
+++ content/blog/blog.11tydata.js
@@ -3,4 +3,9 @@ export default {
 		"posts"
 	],
 	"layout": "layouts/post.njk",
+	eleventyComputed: {
+		// Resolve the `author` slug from frontmatter into the full author object
+		// from _data/authors.js, so templates can use `post.data.author.name` etc.
+		author: (data) => data.authors?.[data.author],
+	},
 };
