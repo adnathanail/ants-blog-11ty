94a5fa9859cd0890444c7e81979438c44f91feeb
Alex Nathanail <7809723+adnathanail@users.noreply.github.com>
2026-08-04 14:51:10 +0200

    Add more posts to regression testing

--- tests/visual/pages.spec.js
+++ tests/visual/pages.spec.js
@@ -3,6 +3,10 @@ import { test, expect } from "@playwright/test";
 const pages = [
 	{ name: "home", path: "/" },
 	{ name: "blog-index", path: "/blog/" },
+	{ name: "post-hilbert-curve", path: "/blog/2022-05-01-hilbert-curve/" },
+	{ name: "post-sock-pairing", path: "/blog/2024-02-24-sock-pairing/" },
+	{ name: "post-advent-of-code-2024", path: "/blog/2024-12-26-advent-of-code-2024/" },
+	{ name: "post-how-many-puzzle-pieces", path: "/blog/2025-01-08-how-many-puzzle-pieces/" },
 	{ name: "post-moving-to-typst", path: "/blog/2025-11-11-moving-to-typst/" },
 	{ name: "404", path: "/404.html" },
 ];
